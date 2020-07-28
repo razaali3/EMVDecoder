c02f3f3fc68379e34dfaf43a619e43f191a0f631
razaali3 <53042254+razaali3@users.noreply.github.com>
2020-07-28 22:57:39 +0530

    Update SimpleTagParser.py

--- SimpleTagParser.py
+++ SimpleTagParser.py
@@ -4,27 +4,28 @@
 simpleTags = {}
 
 class SimpleTag():
-    def __init__(self, name):
+    def __init__(self, name, desc):
         self.name = name
+        self.desc = desc
 
-    def writeTagData(self, tag, desc):
-        simpleTags[tag] = SimpleTag( desc )
+    def writeTagData(self, tag, name, desc):
+        simpleTags[tag] = SimpleTag( name, desc )
 
 file1 = open("SimpleTags.txt","r+") 
 
-print ("Output of Readline function is ")
 lines = file1.readlines()
 for line in lines:
     if ( ( line.isspace() == False ) and ( line.find('*') == -1 ) and ( line.find('#') == -1 ) and ( line.find('/') == -1 )) :
         lineList = line.split(':')
 
         tagID = lineList[0].strip('\n\t ')
-        tagDesc = lineList[1].strip('\n\t ')
+        tagName = lineList[1].strip('\n\t ')
+        tagDesc = ''
+        if len( lineList ) > 2:
+            tagDesc = lineList[2].strip('\n\t ')
 
-        st = SimpleTag('')
-        st.writeTagData(tagID, tagDesc)
-        tag_data = simpleTags[tagID]
-        print(tagID, tag_data.name)
+        st = SimpleTag('','')
+        st.writeTagData(tagID, tagName, tagDesc)
 
 file1.seek(0) 
 file1.close()
