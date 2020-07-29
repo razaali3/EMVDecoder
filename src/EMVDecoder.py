600939c35e55b83831229c23be8ba20ed1295983
razaali3 <53042254+razaali3@users.noreply.github.com>
2020-07-29 12:34:29 +0530

    Update EMVDecoder.py

--- src/EMVDecoder.py
+++ src/EMVDecoder.py
@@ -47,6 +47,11 @@ def decodeTag9F34(tag, value):
         '44': '44 Enciphered PIN verification performed by ICC',
         '42': '42 Enciphered PIN verified online',
         '1F': '1F No CVM required',
+        '05': '05 Encrypted PIN by ICC + signature',
+        '04': '04 Encrypted PIN by ICC',
+        '03': '03 Plain PIN by ICC + signature',
+        '02': '02 Encrypted PIN online',
+        '01': '01 Plain PIN by ICC',
         '00': '00 Fail'
     }   
 
@@ -63,8 +68,8 @@ def decodeTag9F34(tag, value):
     }
 
     tag9F34Byte3 = {
-        '01': '01 Failed',
-        '02': '02 Successful'
+        '02': '02 Successful',
+        '01': '01 Failed'
     }
     setValues = []
     
@@ -106,7 +111,14 @@ def isValidValue(tag, value):
     return ()
 
 def main():
-    inputList = [('9F33', 'E0F0C0'), ('57', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'), ('95', 'FFFFFFFFFF'), ('5F2A', '0978'), ('82', '2000'), ('9F34', '1F0002'), ('8E', '00000000000000004201410342031E031F00')]
+    inputList = [('50', '5649534120435245444954'), ('5A', 'XXXXXXXXXXXXXXXX'), ('5F24', 'XXXXXX'), ('5F25', '090701'), ('5F2A', '0840'),
+    ('5F34', '01'), ('82', '5800'), ('84', 'A0000000031010'), ('8A', '3030'), ('8E', '00000000000000001E0302031F00'), 
+    ('91', 'E17F5CC000800000'), ('95', '0200888000'), ('9A', '200515'), ('9B', 'E800'), ('9C', '00'), ('9F02', '000000001000'), 
+    ('9F03', '000000000000'), ('9F06', 'A0000000031010'), ('9F07', 'FF00'), ('9F08', '0096'), ('9F09', '0096'), ('9F0D', 'F040008800'), 
+    ('9F0E', '0010000000'), ('9F0F', 'F040009800'), ('9F10', 'XXXXXXXXXXXXXX'), ('9F11', '01'), ('9F12', '4352454449544F2044452056495341'), 
+    ('9F15', '5542'), ('9F1A', '0840'), ('9F1B', '00000000'), ('9F1C', '3031393837333532'), ('9F1E', '3031393837333532'), 
+    ('9F21', '115054'), ('9F26', 'DF0BC0A1BE814A49'), ('9F27','80'), ('9F33', '60D8C8'), ('9F34', '020301'), ('9F35', '25'), 
+    ('9F36', '0A03'), ('9F37', '3EC4DD7E'), ('9F39', '05'), ('9F40', '6000F01001'), ('9F41', '0003'), ('9F53', '34')]
     result = decodeTags(inputList)
     print(result)
 
